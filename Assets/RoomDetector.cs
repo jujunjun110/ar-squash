3adc3c416e0a0fd1523ce45dae6c8ee040e9dfac
Jun Ito <jun_ito@voyagegroup.com>
2019-03-03 14:10:02 +0900

    Add: とりあえず動きそうなRoomDetectorを作成

---
+++ Assets/RoomDetector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.iOS;
+public class RoomDetector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+//        planeAnchorMap = new LinkedListDictionary<string,ARPlaneAnchorGameObject> ();
+        UnityARSessionNativeInterface.ARAnchorAddedEvent += AddAnchor;
+//        UnityARSessionNativeInterface.ARAnchorUpdatedEvent += UpdateAnchor;
+//        UnityARSessionNativeInterface.ARAnchorRemovedEvent += RemoveAnchor;       
+    }
+
+    void AddAnchor(ARPlaneAnchor arPlaneAnchor)
+    {
+        Debug.Log("anchor added");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+}
+
