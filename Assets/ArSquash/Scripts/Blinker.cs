635a205461a35237800bec8484d696a0f4e77efa
Jun Ito <jun_ito@voyagegroup.com>
2019-03-07 23:35:57 +0900

    Update: blinker

---
+++ Assets/ArSquash/Scripts/Blinker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blinker : MonoBehaviour {
+    private int count;
+    public bool doBlink = true;
+    public double max_opacity = 0.8;
+    public double min_opacity = 0.4;
+
+    void Start() {
+        count = 0;
+    }
+
+    void Update() {
+        if (!doBlink) {
+            return;
+        }
+
+        count += 5;
+        if (count > 360) {
+            count = 0;
+        }
+
+        var meshRenderer = GetComponent<MeshRenderer>();
+        var opacity_range = (max_opacity - min_opacity) / 2;
+        var opacity = (float) (Math.Sin(count * (Math.PI / 180)) * opacity_range + min_opacity + opacity_range);
+        var _color = meshRenderer.material.color;
+        _color.a = opacity;
+        meshRenderer.material.color = _color;
+    }
+}
