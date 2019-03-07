a68099c70caeca4363f2ee29d82275546951f2d5
Jun Ito <jun_ito@voyagegroup.com>
2019-03-07 23:53:34 +0900

    Update: ゆか

---
+++ Assets/FloorController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorController : MonoBehaviour {
+    public int scale = 1;
+    public int max_scale = 100;
+
+    void Start() {
+        scale = 1;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (scale > max_scale) {
+            return;
+        }
+
+        scale++;
+        transform.localScale = new Vector3(scale, 0.01f, scale);
+    }
+}
