8819d47f1be151c2bcfc12219612739767aeccde
Jun Ito <jun_ito@voyagegroup.com>
2019-03-08 20:33:31 +0900

    Update: デバッグしやすいカメラの状態に

---
+++ Assets/DebugObjectController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugObjectController : MonoBehaviour {
+    [SerializeField] private GameObject debugFloor;
+
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+}
