757b2e0ffc200b2a81d4fff2fe6dc9850e9a6e31
Jun Ito <jun_ito@voyagegroup.com>
2019-03-08 20:50:15 +0900

    Update: なぜかMouseDownがとれない

--- Assets/ArSquash/Scripts/DebugCameraController.cs
+++ Assets/ArSquash/Scripts/DebugCameraController.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class DebugCameraController : MonoBehaviour {
     [SerializeField] private float speed = 10.0f;
+    [SerializeField] public float horizontalSpeed = 2.0F;
+    [SerializeField] public float verticalSpeed = 2.0F;
+
 
     void Update() {
         if (Input.GetAxis("Mouse X") == 0) {
             return;
         }
 
-        transform.position += new Vector3(
-            Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed,
-            0.0f,
-            Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed
-        );
+        if (Input.GetMouseButtonDown(0)) {
+            Debug.Log("MouseDown");
+            transform.position += new Vector3(
+                Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed,
+                0.0f,
+                Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed
+            );
+        } else {
+            Debug.Log("MouseUp");
+            transform.Rotate(
+                verticalSpeed * Input.GetAxis("Mouse Y"),
+                horizontalSpeed * Input.GetAxis("Mouse X"),
+                0
+            );
+        }
     }
 }
