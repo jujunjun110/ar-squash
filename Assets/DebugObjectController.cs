a4f269998a4d4e1fae84d7127cc6ec532dafa427
Jun Ito <jun_ito@voyagegroup.com>
2019-03-08 20:39:41 +0900

    Update: editor modeのときだけ動くオブジェクトたちをついか

--- Assets/DebugObjectController.cs
+++ Assets/DebugObjectController.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DebugObjectController : MonoBehaviour {
-    [SerializeField] private GameObject debugFloor;
+    [SerializeField] private GameObject arCamera;
+//    [SerializeField] private GameObject debugFloor;
 
     void Start() {
+        if (Application.isEditor) {
+            arCamera.SetActive(false);
+//            debugFloor.SetActive(true);
+            foreach (Transform child in transform) {
+                child.gameObject.SetActive(true);
+            }
+        }
     }
 
     // Update is called once per frame
