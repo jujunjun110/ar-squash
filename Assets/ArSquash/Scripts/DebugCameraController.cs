2482df04e978455356912fd748a6ef40d23a5a71
Jun Ito <jun_ito@voyagegroup.com>
2019-03-08 20:23:46 +0900

    Update: debugcamera

--- Assets/ArSquash/Scripts/DebugCameraController.cs
+++ Assets/ArSquash/Scripts/DebugCameraController.cs
@@ -3,21 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DebugCameraController : MonoBehaviour {
-    float speed = 10.0f;
+    [SerializeField] private float speed = 10.0f;
 
     void Update() {
-        if (Input.GetAxis("Mouse X") > 0) {
-            transform.position += new Vector3(
-                Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed,
-                0.0f, 
-                Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed
-            );
-        } else if (Input.GetAxis("Mouse X") < 0) {
-            transform.position += new Vector3(
-                Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed,
-                0.0f, 
-                Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed
-            );
+        if (Input.GetAxis("Mouse X") == 0) {
+            return;
         }
+
+        transform.position += new Vector3(
+            Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed,
+            0.0f,
+            Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed
+        );
     }
 }
