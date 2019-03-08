f8a801060bc6c1cc8472b04f3909749268383f95
Jun Ito <jun_ito@voyagegroup.com>
2019-03-08 22:07:17 +0900

    Update: EditorのTouchにも対応

--- Assets/ArSquash/Scripts/CursorController.cs
+++ Assets/ArSquash/Scripts/CursorController.cs
@@ -4,12 +4,7 @@ public class CursorController : MonoBehaviour {
     [SerializeField] private GameObject PolePrefab;
 
     void Update() {
-        if (Input.touchCount <= 0) {
-            return;
-        }
-
-        var touch = Input.GetTouch(0);
-        if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved) {
+        if (!touchEnd()) {
             return;
         }
 
@@ -18,5 +13,22 @@ public class CursorController : MonoBehaviour {
         var pos = transform.position;
         pole.transform.localPosition = new Vector3(pos.x, pos.y + poleHeight, pos.z);
         pole.transform.rotation = transform.rotation;
+    }
+
+    bool touchEnd() {
+        if (Application.isEditor) {
+            return Input.GetMouseButtonUp(0);
+        }
+
+        if (Input.touchCount <= 0) {
+            return false;
+        }
+
+        var touch = Input.GetTouch(0);
+        if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved) {
+            return false;
+        }
+
+        return true;
     }
 }
