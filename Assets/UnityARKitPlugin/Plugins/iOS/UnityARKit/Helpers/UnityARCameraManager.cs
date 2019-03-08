b663928e95cb6f2bf4dad486d8522b827c494a19
Jun Ito <jun_ito@voyagegroup.com>
2019-03-08 20:14:34 +0900

    Update: format

--- Assets/UnityARKitPlugin/Plugins/iOS/UnityARKit/Helpers/UnityARCameraManager.cs
+++ Assets/UnityARKitPlugin/Plugins/iOS/UnityARKit/Helpers/UnityARCameraManager.cs
@@ -4,32 +4,28 @@ using UnityEngine;
 using UnityEngine.XR.iOS;
 
 public class UnityARCameraManager : MonoBehaviour {
-
     public Camera m_camera;
     private UnityARSessionNativeInterface m_session;
     private Material savedClearMaterial;
 
-    [Header("AR Config Options")]
-    public UnityARAlignment startAlignment = UnityARAlignment.UnityARAlignmentGravity;
+    [Header("AR Config Options")] public UnityARAlignment startAlignment = UnityARAlignment.UnityARAlignmentGravity;
     public UnityARPlaneDetection planeDetection = UnityARPlaneDetection.Horizontal;
     public bool getPointCloud = true;
     public bool enableLightEstimation = true;
     public bool enableAutoFocus = true;
-	public UnityAREnvironmentTexturing environmentTexturing = UnityAREnvironmentTexturing.UnityAREnvironmentTexturingNone;
 
-    [Header("Image Tracking")]
-    public ARReferenceImagesSet detectionImages = null;
+    public UnityAREnvironmentTexturing environmentTexturing =
+        UnityAREnvironmentTexturing.UnityAREnvironmentTexturingNone;
+
+    [Header("Image Tracking")] public ARReferenceImagesSet detectionImages = null;
     public int maximumNumberOfTrackedImages = 0;
 
-    [Header("Object Tracking")]
-    public ARReferenceObjectsSetAsset detectionObjects = null;
+    [Header("Object Tracking")] public ARReferenceObjectsSetAsset detectionObjects = null;
     private bool sessionStarted = false;
 
-    public ARKitWorldTrackingSessionConfiguration sessionConfiguration
-    {
-        get
-        {
-            ARKitWorldTrackingSessionConfiguration config = new ARKitWorldTrackingSessionConfiguration ();
+    public ARKitWorldTrackingSessionConfiguration sessionConfiguration {
+        get {
+            ARKitWorldTrackingSessionConfiguration config = new ARKitWorldTrackingSessionConfiguration();
             config.planeDetection = planeDetection;
             config.alignment = startAlignment;
             config.getPointCloudData = getPointCloud;
@@ -40,26 +36,25 @@ public class UnityARCameraManager : MonoBehaviour {
             if (detectionImages != null)
                 config.referenceImagesGroupName = detectionImages.resourceGroupName;
 
-			if (detectionObjects != null) 
-			{
-				config.referenceObjectsGroupName = "";  //lets not read from XCode asset catalog right now
-				config.dynamicReferenceObjectsPtr = m_session.CreateNativeReferenceObjectsSet(detectionObjects.LoadReferenceObjectsInSet());
-			}
+            if (detectionObjects != null) {
+                config.referenceObjectsGroupName = ""; //lets not read from XCode asset catalog right now
+                config.dynamicReferenceObjectsPtr =
+                    m_session.CreateNativeReferenceObjectsSet(detectionObjects.LoadReferenceObjectsInSet());
+            }
 
             return config;
         }
     }
 
     // Use this for initialization
-    void Start () {
-
+    void Start() {
         m_session = UnityARSessionNativeInterface.GetARSessionNativeInterface();
 
         Application.targetFrameRate = 60;
-        
+
         var config = sessionConfiguration;
         if (config.IsSupported) {
-            m_session.RunWithConfig (config);
+            m_session.RunWithConfig(config);
             UnityARSessionNativeInterface.ARFrameUpdatedEvent += FirstFrameUpdate;
         }
 
@@ -68,58 +63,52 @@ public class UnityARCameraManager : MonoBehaviour {
         }
     }
 
-    void OnDestroy()
-    {
+    void OnDestroy() {
         m_session.Pause();
     }
 
-    void FirstFrameUpdate(UnityARCamera cam)
-    {
+    void FirstFrameUpdate(UnityARCamera cam) {
         sessionStarted = true;
         UnityARSessionNativeInterface.ARFrameUpdatedEvent -= FirstFrameUpdate;
     }
 
-    public void SetCamera(Camera newCamera)
-    {
+    public void SetCamera(Camera newCamera) {
         if (m_camera != null) {
-            UnityARVideo oldARVideo = m_camera.gameObject.GetComponent<UnityARVideo> ();
+            UnityARVideo oldARVideo = m_camera.gameObject.GetComponent<UnityARVideo>();
             if (oldARVideo != null) {
                 savedClearMaterial = oldARVideo.m_ClearMaterial;
-                Destroy (oldARVideo);
+                Destroy(oldARVideo);
             }
         }
-        SetupNewCamera (newCamera);
+
+        SetupNewCamera(newCamera);
     }
 
-    private void SetupNewCamera(Camera newCamera)
-    {
+    private void SetupNewCamera(Camera newCamera) {
         m_camera = newCamera;
 
         if (m_camera != null) {
-            UnityARVideo unityARVideo = m_camera.gameObject.GetComponent<UnityARVideo> ();
+            UnityARVideo unityARVideo = m_camera.gameObject.GetComponent<UnityARVideo>();
             if (unityARVideo != null) {
                 savedClearMaterial = unityARVideo.m_ClearMaterial;
-                Destroy (unityARVideo);
+                Destroy(unityARVideo);
             }
-            unityARVideo = m_camera.gameObject.AddComponent<UnityARVideo> ();
+
+            unityARVideo = m_camera.gameObject.AddComponent<UnityARVideo>();
             unityARVideo.m_ClearMaterial = savedClearMaterial;
         }
     }
 
     // Update is called once per frame
 
-    void Update () {
-        
-        if (m_camera != null && sessionStarted)
-        {
+    void Update() {
+        if (m_camera != null && sessionStarted) {
             // JUST WORKS!
             Matrix4x4 matrix = m_session.GetCameraPose();
             m_camera.transform.localPosition = UnityARMatrixOps.GetPosition(matrix);
-            m_camera.transform.localRotation = UnityARMatrixOps.GetRotation (matrix);
+            m_camera.transform.localRotation = UnityARMatrixOps.GetRotation(matrix);
 
-            m_camera.projectionMatrix = m_session.GetCameraProjection ();
+            m_camera.projectionMatrix = m_session.GetCameraProjection();
         }
-
     }
-
 }
