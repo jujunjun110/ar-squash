0b8287acd65ea2641c55757a9225fedf65d6b5ff
Jun Ito <jun_ito@voyagegroup.com>
2019-03-11 20:36:59 +0900

    Update: ソースコードとしては動いているが...

--- Assets/ArSquash/Scripts/RoomGenerator.cs
+++ Assets/ArSquash/Scripts/RoomGenerator.cs
@@ -1,9 +1,18 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 public class RoomGenerator : MonoBehaviour {
     [SerializeField] private GameObject cursorObject;
     [SerializeField] private GameObject polePrefab;
     [SerializeField] private GameObject room;
+
+    private List<Transform> tappedPoints = new List<Transform>();
+
+
+    void Start() {
+//        tappedPoints = new List<Transform>();
+    }
 
 
     void Update() {
@@ -23,5 +32,30 @@ public class RoomGenerator : MonoBehaviour {
             cursorObject.transform.rotation,
             room.transform
         );
+
+        tappedPoints.Add(cursorObject.transform);
+        Debug.Log(tappedPoints.Count);
+        if (tappedPoints.Count >= 2) {
+            var point1 = tappedPoints[0].position;
+            var point2 = tappedPoints[1].position;
+            var point3 = new Vector3(point1.x, point1.y + 100, point1.z);
+
+            Debug.Log(point1);
+            Debug.Log(point2);
+            Debug.Log(point3);
+
+            var mesh = new Mesh {
+                vertices = new Vector3[] {
+                    point1,
+                    point2,
+                    point3
+                },
+                triangles = new int[] {0, 1, 2}
+            };
+
+            mesh.RecalculateNormals();
+            var filter = GetComponent<MeshFilter>();
+            filter.sharedMesh = mesh;
+        }
     }
 }
