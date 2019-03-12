5d2c56e335110eb960c605a68ef135fe568a0e62
Jun Ito <jun_ito@voyagegroup.com>
2019-03-12 14:23:41 +0900

    Add: テスト

---
+++ Assets/ArSquash/Editor/SampleTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests {
+    public class SampleTest {
+        [Test]
+        public void AnyTest() {
+            int a = 5, b = 4;
+            int ans = a * b;
+            Assert.AreEqual(ans, 20);
+            // Use the Assert class to test conditions
+        }
+    }
+}
