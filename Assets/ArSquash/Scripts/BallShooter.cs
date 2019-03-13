e69a7c23e299e7bc9a023a639f2fd066719bbf47
Jun Ito <jun_ito@voyagegroup.com>
2019-03-13 22:11:27 +0900

    Update: ボールをシュートできるように

---
+++ Assets/ArSquash/Scripts/BallShooter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallShooter : MonoBehaviour {
+    [SerializeField] private GameObject ballPrefab;
+    private GameObject ball;
+
+    private void FixedUpdate() {
+        if (!Input.GetKeyUp(KeyCode.Space)) {
+            return;
+        }
+
+        Debug.Log("Space Pushed");
+
+//        if (ball) {
+//            return;
+//        }
+
+        ball = Instantiate(ballPrefab);
+        ball.transform.position = transform.position;
+        var speed = 10.0f;
+        var vec = this.transform.forward * speed;
+        ball.GetComponent<Rigidbody>().velocity = vec;
+    }
+}
